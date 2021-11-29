2aed82895b874e8fb46289bb5715ad71301a7cc7
goose <44867294+60053Bumps@users.noreply.github.com>
2021-11-29 17:43:51 +0100

    new

---
+++ front/hackooo/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="home">
+      <Intro />
+  </div>
+</template>
+
+<script>
+import Intro from "../components/intro.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Intro
+  },
+  data() {
+    return {
+      
+    };
+  },
+  computed: {
+    
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
