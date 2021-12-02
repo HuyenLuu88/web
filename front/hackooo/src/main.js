3ac5c3ba6c85ba07ba61ca6bead4ce553c60ad85
goose <44867294+60053Bumps@users.noreply.github.com>
2021-12-02 03:38:36 +0100

    test

--- front/hackooo/src/main.js
+++ front/hackooo/src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import axios from './axios'
 import store from './store'
-
+import LottieAnimation from "lottie-vuejs/src/LottieAnimation.vue"; // import lottie-vuejs
+import { gsap } from "gsap";
 
 const app = createApp(App)
+app.use(gsap)
+app.use(LottieAnimation)
 app.use(router, axios)
 app.use(store)
 
